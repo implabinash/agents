11e4809e17f0e75fce2d75232f478c6e6c20e40b
Abinash Sahoo <implabinash@gmail.com>
2025-09-14 14:14:11 +0530

    fix: make it dynamic by adding a loop

--- personal_finance/main.js
+++ personal_finance/main.js
@@ -16,58 +16,8 @@ const callAgent = async () => {
         content: "How much money I have spent this month?",
     });
 
-    const completion = await groq.chat.completions.create({
-        messages: messages,
-        model: "openai/gpt-oss-20b",
-        tools: [
-            {
-                type: "function",
-                function: {
-                    name: "getTotalExpense",
-                    description: "Get total expense from date to date",
-                    parameters: {
-                        type: "object",
-                        properties: {
-                            from: {
-                                type: "string",
-                                description: "From date to get the expense",
-                            },
-                            to: {
-                                type: "string",
-                                description: "To date to get the expense",
-                            },
-                        },
-                    },
-                },
-            },
-        ],
-    });
-
-    messages.push(completion.choices[0].message);
-
-    const toolCalles = completion.choices[0].message.tool_calls;
-
-    if (!toolCalles) {
-        console.log(completion.choices[0].message.content);
-        return;
-    }
-
-    for (const tool of toolCalles) {
-        const name = tool.function.name;
-        const args = tool.function.arguments;
-
-        let result = "";
-        if (name === "getTotalExpense") {
-            result = getTotalExpense(JSON.parse(args));
-        }
-
-        messages.push({
-            role: "tool",
-            content: result,
-            tool_call_id: tool.id,
-        });
-
-        const completion2 = await groq.chat.completions.create({
+    while (true) {
+        const completion = await groq.chat.completions.create({
             messages: messages,
             model: "openai/gpt-oss-20b",
             tools: [
@@ -94,7 +44,30 @@ const callAgent = async () => {
             ],
         });
 
-        console.log(completion2.choices[0].message.content);
+        messages.push(completion.choices[0].message);
+
+        const toolCalles = completion.choices[0].message.tool_calls;
+
+        if (!toolCalles) {
+            console.log(completion.choices[0].message.content);
+            break;
+        }
+
+        for (const tool of toolCalles) {
+            const name = tool.function.name;
+            const args = tool.function.arguments;
+
+            let result = "";
+            if (name === "getTotalExpense") {
+                result = getTotalExpense(JSON.parse(args));
+            }
+
+            messages.push({
+                role: "tool",
+                content: result,
+                tool_call_id: tool.id,
+            });
+        }
     }
 };
 
