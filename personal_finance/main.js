51326b9609b57072749753561621b9811af7c05f
Abinash Sahoo <implabinash@gmail.com>
2025-09-14 14:45:25 +0530

    feat: make it dynamic to get user prompth

--- personal_finance/main.js
+++ personal_finance/main.js
@@ -1,10 +1,16 @@
 import Groq from "groq-sdk";
+import readline from "node:readline/promises";
 
 const groq = new Groq({ apiKey: process.env.AGENT_KEY });
 
 let expenseDB = [];
 
 const callAgent = async () => {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
     let messages = [
         {
             role: "system",
@@ -13,87 +19,97 @@ const callAgent = async () => {
         },
     ];
 
-    messages.push({
-        role: "user",
-        content: "I baught a new macbook pro for 400000.",
-    });
-
     while (true) {
-        const completion = await groq.chat.completions.create({
-            messages: messages,
-            model: "openai/gpt-oss-20b",
-            tools: [
-                {
-                    type: "function",
-                    function: {
-                        name: "getTotalExpense",
-                        description: "Get total expense from date to date",
-                        parameters: {
-                            type: "object",
-                            properties: {
-                                from: {
-                                    type: "string",
-                                    description: "From date to get the expense",
-                                },
-                                to: {
-                                    type: "string",
-                                    description: "To date to get the expense",
-                                },
-                            },
-                        },
-                    },
-                },
-                {
-                    type: "function",
-                    function: {
-                        name: "addExpense",
-                        description:
-                            "Add a new expense to the expense database",
-                        parameters: {
-                            type: "object",
-                            properties: {
-                                name: {
-                                    type: "string",
-                                    description:
-                                        "Name of the expense, e.g., Baught a iPhone",
-                                },
-                                amount: {
-                                    type: "string",
-                                    description: "Amount of the expense",
-                                },
-                            },
-                        },
-                    },
-                },
-            ],
-        });
+        const query = await rl.question("User: ");
 
-        messages.push(completion.choices[0].message);
-
-        const toolCalles = completion.choices[0].message.tool_calls;
-
-        if (!toolCalles) {
-            console.log(completion.choices[0].message.content);
+        if (query === "/exit") {
             break;
         }
 
-        for (const tool of toolCalles) {
-            const name = tool.function.name;
-            const args = tool.function.arguments;
+        messages.push({
+            role: "user",
+            content: query,
+        });
 
-            let result = "";
+        while (true) {
+            const completion = await groq.chat.completions.create({
+                messages: messages,
+                model: "openai/gpt-oss-20b",
+                tools: [
+                    {
+                        type: "function",
+                        function: {
+                            name: "getTotalExpense",
+                            description: "Get total expense from date to date",
+                            parameters: {
+                                type: "object",
+                                properties: {
+                                    from: {
+                                        type: "string",
+                                        description:
+                                            "From date to get the expense",
+                                    },
+                                    to: {
+                                        type: "string",
+                                        description:
+                                            "To date to get the expense",
+                                    },
+                                },
+                            },
+                        },
+                    },
+                    {
+                        type: "function",
+                        function: {
+                            name: "addExpense",
+                            description:
+                                "Add a new expense to the expense database",
+                            parameters: {
+                                type: "object",
+                                properties: {
+                                    name: {
+                                        type: "string",
+                                        description:
+                                            "Name of the expense, e.g., Baught a iPhone",
+                                    },
+                                    amount: {
+                                        type: "string",
+                                        description: "Amount of the expense",
+                                    },
+                                },
+                            },
+                        },
+                    },
+                ],
+            });
 
-            if (name === "getTotalExpense") {
-                result = getTotalExpense(JSON.parse(args));
-            } else if (name === "addExpense") {
-                result = addExpense(JSON.parse(args));
+            messages.push(completion.choices[0].message);
+
+            const toolCalles = completion.choices[0].message.tool_calls;
+
+            if (!toolCalles) {
+                console.log(completion.choices[0].message.content);
+                break;
             }
 
-            messages.push({
-                role: "tool",
-                content: result,
-                tool_call_id: tool.id,
-            });
+            for (const tool of toolCalles) {
+                const name = tool.function.name;
+                const args = tool.function.arguments;
+
+                let result = "";
+
+                if (name === "getTotalExpense") {
+                    result = getTotalExpense(JSON.parse(args));
+                } else if (name === "addExpense") {
+                    result = addExpense(JSON.parse(args));
+                }
+
+                messages.push({
+                    role: "tool",
+                    content: result,
+                    tool_call_id: tool.id,
+                });
+            }
         }
     }
 
