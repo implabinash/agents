ca04320748ee4ea3d561e005f7366db8b35e55d2
Abinash Sahoo <implabinash@gmail.com>
2025-09-14 12:42:16 +0530

    feat: add the first tool

--- personal_finance/main.js
+++ personal_finance/main.js
@@ -3,35 +3,50 @@ import Groq from "groq-sdk";
 const groq = new Groq({ apiKey: process.env.AGENT_KEY });
 
 const callAgent = async () => {
-    const completion = await groq.chat.completions
-        .create({
-            messages: [
-                {
-                    role: "system",
-                    content:
-                        "You are Josh, a personal assistant. You task is to assist user with expenses, balances and finacial planning.",
+    const completion = await groq.chat.completions.create({
+        messages: [
+            {
+                role: "system",
+                content: `You are Josh, a personal assistant. You task is to assist user with expenses, balances and finacial planning.
+                    Current datetime: ${new Date().toUTCString()}`,
+            },
+            {
+                role: "user",
+                content: "How much money I have spent this month?",
+            },
+        ],
+        model: "openai/gpt-oss-20b",
+        tools: [
+            {
+                type: "function",
+                function: {
+                    name: "getTotalExpense",
+                    description: "Get total expense from date to date",
+                    parameters: {
+                        type: "object",
+                        properties: {
+                            from: {
+                                type: "string",
+                                description: "From date to get the expense",
+                            },
+                            to: {
+                                type: "string",
+                                description: "To date to get the expense",
+                            },
+                        },
+                    },
                 },
-                {
-                    role: "user",
-                    content: "Who are you?",
-                },
-            ],
-            model: "openai/gpt-oss-20b",
-            tools: [
-                {
-                    type: "function",
-                },
-            ],
-        })
-        .then((chatCompletion) => {
-            console.log(chatCompletion.choices[0]?.message?.content || "");
-        });
+            },
+        ],
+    });
+
+    console.log(JSON.stringify(completion.choices[0], null, 2));
 };
 
 callAgent();
 
 const getTotalExpense = ({ from, to }) => {
-    console.log("calling getTotalExpense tool");
+    console.log("calling getTotalExpense tool. From: ", from, "To: ", to);
 
     return 10_000;
 };
