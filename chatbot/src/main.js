b1679fe74a59b90c491016cc374c5dcca0ef706e
Abinash Sahoo <implabinash@gmail.com>
2025-09-15 11:49:11 +0530

    feat: basic chatbot complete

--- chatbot/src/main.js
+++ chatbot/src/main.js
@@ -1,4 +1,26 @@
+import { MessagesAnnotation, StateGraph } from "@langchain/langgraph";
+import { ChatGroq } from "@langchain/groq";
+
 import readline from "node:readline/promises";
+
+const llm = new ChatGroq({
+    model: "openai/gpt-oss-20b",
+    temperature: 0,
+    maxRetries: 2,
+    apiKey: process.env.AGENT_KEy,
+});
+
+const callModel = async (state) => {
+    const response = await llm.invoke(state.messages);
+    return { messages: [response] };
+};
+
+const workflow = new StateGraph(MessagesAnnotation)
+    .addNode("agent", callModel)
+    .addEdge("__start__", "agent")
+    .addEdge("agent", "__end__");
+
+const app = workflow.compile();
 
 const main = async () => {
     const rl = readline.createInterface({
@@ -14,7 +36,13 @@ const main = async () => {
             break;
         }
 
-        console.log(`${query}\n`);
+        const finastate = await app.invoke({
+            messages: [{ role: "user", content: query }],
+        });
+
+        const aiReply = finastate.messages[finastate.messages.length - 1];
+
+        console.log("AI: ", aiReply.content);
     }
 };
 
